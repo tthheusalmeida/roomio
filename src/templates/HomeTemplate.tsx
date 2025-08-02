550af67e3b4e1ff216f0b47bcdb1c6cdd448e42c
Matheus Almeida <tthheusalmeida@gmail.com>
2025-08-02 12:22:48 -0300

    chore: remove min-height

--- src/templates/HomeTemplate.tsx
+++ src/templates/HomeTemplate.tsx
@@ -2,7 +2,7 @@ import Title from "@/components/atoms/Title";
 
 export default function HomeTemplate() {
   return (
-    <div className="w-full flex flex-col min-h-dvh">
+    <div className="w-full flex flex-col">
       <Title>Home</Title>
     </div>
   );
--- src/templates/RankingTemplate.tsx
+++ src/templates/RankingTemplate.tsx
@@ -2,7 +2,7 @@ import Title from "@/components/atoms/Title";
 
 export default function RankingTemplate() {
   return (
-    <div className="w-full flex flex-col min-h-dvh">
+    <div className="w-full flex flex-col">
       <Title>Ranking</Title>
     </div>
   );
