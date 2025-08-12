eda2b2c478c7a40232bbb5302c3a1d5fe86e23cc
Matheus Almeida <tthheusalmeida@gmail.com>
2025-08-12 11:28:20 -0300

    chore: change Waiting for players field id to name

--- src/components/molecules/ConectedPlayers.tsx
+++ src/components/molecules/ConectedPlayers.tsx
@@ -1,5 +1,6 @@
 import { mergeClassNames } from "@/utils/classNames";
 import ConnectedPlayer from "../atoms/ConnectedPlayer";
+import LoadingLabel from "../atoms/LoadingLabel";
 
 interface ConnectedPlayersProps {
   players: string[];
@@ -12,9 +13,11 @@ export default function ConnectedPlayers({
 }: ConnectedPlayersProps) {
   return (
     <div className={mergeClassNames(className)}>
-      <span className="animate-pulse text-xs">Connecting players...</span>
+      <LoadingLabel label="Waiting for players..." />
 
       <div className="mt-2 flex flex-col gap-2 p-4 bg-violet-900 rounded-lg">
+        <span className="text-sm self-start">Connected:</span>
+
         {players.map((name, index) => (
           <ConnectedPlayer key={index} name={name} />
         ))}
